1df998a9ddcac1567a1d2a152d296170f8edc8de
Jon Fuller <jcfuller@sep.com>
2017-12-07 12:58:12 -0500

    day 7, part 2

--- 7/circus.rb
+++ 7/circus.rb
@@ -1,5 +1,9 @@
 require 'pp'
 
+def debug(str)
+  pp str
+end
+
 def load_input(filename)
   File.read(filename).lines.map{|l| l.chomp}.map{|l|
     splits = l.split(' ')
@@ -30,16 +34,27 @@ def find_weight(proc_name, proc_list)
   process[:weight] + process[:children].inject(0){|sum, child| sum += find_weight(child, proc_list)}
 end
 
+def bad_child(proc_name, proc_list)
+  root = proc_for(proc_name, proc_list)
+
+  children = root[:children]
+    .map{|c| {name: c, weight: proc_for(c, proc_list)[:weight], total_weight: find_weight(c, proc_list)}}
+    .group_by{|c| c[:total_weight]}
+    .sort_by{|weight, children| children.length}
+
+  off_balance = children[0][1].first
+  off_by = children[0][0] - children[1][0]
+
+  debug("-------")
+  debug(children)
+  debug(off_by)
+
+  bad_child(off_balance[:name], proc_list)
+end
 
 sample = load_input('sample')
 input = load_input('input')
 
-pp sample
-
-pp find_root(sample)
 pp find_root(input)
 
-pp find_weight(find_root(sample)[:root], sample)
-pp find_weight('ugml', sample)
-pp find_weight('padx', sample)
-pp find_weight('fwft', sample)
+pp bad_child(find_root(input)[:root], input)
