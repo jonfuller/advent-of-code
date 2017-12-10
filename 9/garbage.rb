dffb9c12f12a8c724099b97b987b13b666628cfa
Jon Fuller <jcfuller@sep.com>
2017-12-09 23:38:55 -0500

    day 9, part 2, counting trash

--- 9/garbage.rb
+++ 9/garbage.rb
@@ -16,6 +16,18 @@ def samples
   ]
 end
 
+def trash_samples
+  [
+    {input: '<>', trash: 0},
+    {input: '<random characters>', trash: 17 },
+    {input: '<<<<>', trash: 3},
+    {input: '<{!>}>', trash: 2},
+    {input: '<!!>', trash: 0},
+    {input: '<!!!>>', trash: 0},
+    {input: '<{o"i!a,<{i<a>', trash: 10},
+  ]
+end
+
 def input
   File.read('input')
 end
@@ -23,18 +35,21 @@ end
 def parse(input)
   score = 0
   groups = 0
+  trash_count = 0
   state = [:NONE]
   input = input.chars
   while(input.length > 0)
     current_input = input.first
     input = input.drop(1)
-    debug("#{state} - #{current_input} - #{state.length}")
+    debug("#{state} - #{current_input} - #{state.length} - #{trash_count}")
     
     case state.last
     when :NONE
       case current_input
       when '{'
         state.push :WITHIN_GROUP
+      when '<'
+        state.push :WITHIN_TRASH
       end
     when :WITHIN_GROUP
       case current_input
@@ -56,16 +71,21 @@ def parse(input)
       when '!'
         state.push :CANCEL
       else
+        trash_count += 1
       end
     when :CANCEL
       state.pop
     end
   end
-  {groups: groups, score: score}
+  {groups: groups, score: score, trash_count: trash_count}
 end
 
 samples.each do |sample|
   pp "-----", {groups: sample[:groups], score: sample[:score]}, parse(sample[:input])
+end
+
+trash_samples.each do |sample|
+  pp "-----", sample, parse(sample[:input])[:trash_count]
 end
 
 pp parse(input)
