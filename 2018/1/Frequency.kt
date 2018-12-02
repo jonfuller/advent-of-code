69f5ec0aff3a482d8a763a3057ca3c2ee1e3c4e1
Jon Fuller <fullerjc@gmail.com>
2018-12-02 11:47:51 -0500

    second part

--- 2018/1/Frequency.kt
+++ 2018/1/Frequency.kt
@@ -5,7 +5,41 @@ fun main(args: Array<String>) {
         .readLines()
         .map { l -> l.toInt() }
         ;
-    
-    var sum = input.reduce { total, next -> total + next }
-    println(sum)
+
+    var freqMap = hashSetOf<Int>(0)
+
+    var result = Result(0, null);
+    while (result.repeating == null) {
+        result = findRepeat(result.ending, input, freqMap);
+    }
+
+    println(result.repeating?.toString())
+    println(result.ending)
+}
+
+data class Result(val ending: Int, val repeating: Int?)
+
+fun findRepeat(starting: Int, input: List<Int>, freqMap: HashSet<Int>): Result {
+    var repeat :Int? = null
+    var sum = input.fold(starting) { total, next ->
+        var freq = total + next
+        
+        if (freqMap.contains(freq) && repeat == null)
+            repeat = freq
+        else
+            freqMap.add(freq)
+
+        freq
+    }
+    return Result(sum, repeat)
+}
+
+fun debugLoop(total: Int, next: Int, freq: Int) {
+    debug(total.toString())
+    debug(next.toString())
+    debug(freq.toString())
+    debug("----")
+}
+fun debug(arg: String) {
+    println(arg);
 }
