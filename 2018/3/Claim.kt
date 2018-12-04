73bad0781de7c6c77a7f2bc9846bba33a4a99f9c
Jon Fuller <jcfuller@sep.com>
2018-12-04 08:04:15 -0500

    day 3, part 2

--- 2018/3/Claim.kt
+++ 2018/3/Claim.kt
@@ -8,15 +8,24 @@ import java.io.File;
         var width = claims.maxBy { it.x }!!.x + claims.maxBy { it.width }!!.width - 1
         var height = claims.maxBy { it.y }!!.y + claims.maxBy { it.height }!!.height - 1
 
-
         var fabric = generateSequence { generateSequence { 0 }.take(width).toList().asIterable() }.take(height).toList().asIterable()
 
         fabric = claims.fold(fabric) { acc, claim -> claimFabric(acc, claim)}
 
+        var pureClaims = claims.filter { claim -> getClaim(fabric, claim).flatten().all{ it == 1 } }
+
         println(fabric.flatten().count { it > 1 })
+        pureClaims.map { it.id }.forEach(::println)
     }
 
-data class Claim(val x: Int, val y: Int, val height: Int, val width: Int)
+data class Claim(val id: String, val x: Int, val y: Int, val height: Int, val width: Int)
+
+fun getClaim(fabric: Iterable<Iterable<Int>>, claim: Claim): Iterable<Iterable<Int>> {
+    return fabric
+        .drop(claim.y)
+        .take(claim.height)
+        .map {it.drop(claim.x).take(claim.width)}
+}
 
 fun claimFabric(fabric: Iterable<Iterable<Int>>, claim: Claim): Iterable<Iterable<Int>> {
     return sequence {
@@ -39,7 +48,7 @@ fun parseClaim(line: String): Claim {
     var location = spaced[2].dropLast(1).split(",")
     var size = spaced[3].split("x")
 
-    return Claim(location[0].toInt(), location[1].toInt(), size[1].toInt(), size[0].toInt())
+    return Claim(spaced[0].drop(1), location[0].toInt(), location[1].toInt(), size[1].toInt(), size[0].toInt())
 }
 
 fun debug(fabric: Iterable<Iterable<Int>>) {
